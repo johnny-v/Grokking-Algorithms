ba8848d12fbba967f6856933e588ce981d645480
ivmit <ionutvalentin66@gmail.com>
2021-03-20 16:01:25 +0200

    G-0001: fixing quickSort to O(nlogn)

--- quickSort.js
+++ quickSort.js
@@ -5,7 +5,12 @@ function quickSort(array) {
     }
 
     //choosing the pivot
-    const [pivot, ...remaining] = array;
+    // const [pivot, ...remaining] = array; this includes the worst case;
+
+    //this is to fix from O(n) to O(n log n);
+    const randomIndex =  Math.floor(Math.random() * array.length);
+    const pivot = array[randomIndex];
+    const remaining = array.filter((e, i) => i !== randomIndex);
     //divide and conquer
     const less = remaining.filter(e => e <= pivot);
     const greater = remaining.filter(e => e > pivot);
@@ -18,3 +23,6 @@ console.log(quickSort([1, 5, 21, 44, 0, 2, 3, 4.5]));
 console.log(quickSort([]));
 console.log(quickSort([433434, 4]));
 console.log(quickSort([433434, -4, 2323]));
+
+//selection sort is faster then merge sort if bot On notations are O(nlogn). However there is a case in quick sort that can be O(n)
+// to avoid this and enable quick sort to be on average case and not on worst case, the pivot must be selected randomly.
